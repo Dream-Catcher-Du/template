48b6a7f1d4a398cc6ee8dbd6da322c4cf51244d3
jiaming.du <1481794223@qq.com>
2024-06-14 12:09:11 +0800

    测试

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// 开放所有文件
-app.use('/public', express.static('public'))
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -12,3 +12,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`服务器启动成功`);
 })
+ 
